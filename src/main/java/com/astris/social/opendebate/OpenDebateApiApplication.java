92e44de2c95f779004a17b26fb8a0fd46cf83106
Christopher Bridges <importedsample@yahoo.com>
2020-12-07 22:06:02 -0500

    initial commit

---
+++ src/main/java/com/astris/social/opendebate/OpenDebateApiApplication.java
@@ -0,0 +1,13 @@
+package com.astris.social.opendebate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDebateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDebateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astris/social/opendebate/OpenDebateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astris.social.opendebate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDebateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
